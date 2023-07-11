a61a8accb52d3750b28768fc5047fa93a4a1a946
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2023-07-11 14:51:27 -0300

    Get post by id

---
+++ src/main/java/com/marlupatrocinio/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.marlupatrocinio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marlupatrocinio.workshopmongo.domain.Post;
+import com.marlupatrocinio.workshopmongo.repository.PostRepository;
+import com.marlupatrocinio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+	}
+}
