9e3b9a4ff110b2305de1768794636be3c05bf28c
Marlu Patrocinio <marlupatrocinio01@gmail.com>
2023-07-12 07:09:23 -0300

    Search with @Query

--- src/main/java/com/marlupatrocinio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marlupatrocinio/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marlupatrocinio.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marlupatrocinio.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.marlupatrocinio.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> SearchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
